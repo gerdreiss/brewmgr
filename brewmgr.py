603fb248f8b25d985dc7935f73f1bb11e52d431e
Gerd Reiss <gerdreiss@jscriptive.com>
2018-01-13 18:41:02 +0100

    minor fix + refactoring

--- brewmgr.py
+++ brewmgr.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import subprocess
 import sys
 
@@ -6,29 +8,33 @@ def main():
     if len(sys.argv) < 2:
         print_help()
 
-    arg = sys.argv[1]
-    if arg == "deps":
-        elms = subprocess.check_output(["brew", "list"]).split('\n')
-        print("looking to cleanup from the list\n{}".format(", ".join(elms)))
-        for elm in elms:
-            if elm != "":
-                print("checking usage of {}".format(elm))
-                deps = subprocess.check_output(["brew", "uses", "--installed", elm]).split('\n')
-                if "".join(deps) == "":
-                    remove = raw_input("nothing is using {}. uninstall? (N/y)".format(elm))
-                    if remove == "y":
-                        subprocess.call(["brew", "uninstall", elm])
-                    else:
-                        print("continuing...")
-                else:
-                    print("{} is used by {}. continuing...".format(elm, ", ".join(deps)))
-            else:
-                print("cleaning up...")
-                subprocess.call(["brew", "cleanup"])
-                print("done.")
-
     else:
-        print_help()
+        arg = sys.argv[1]
+        if arg == "deps":
+            elms = subprocess.check_output(["brew", "list"]).split('\n')
+            print("looking to cleanup from the list\n{}".format(", ".join(elms)))
+
+            for elm in elms:
+                if elm == "":
+                    print("cleaning up...")
+                    subprocess.call(["brew", "cleanup"])
+                    print("done.")
+
+                else:
+                    print("checking usage of {}".format(elm))
+                    deps = subprocess.check_output(["brew", "uses", "--installed", elm]).split('\n')
+
+                    if "".join(deps) == "":
+                        remove = raw_input("nothing is using {}. uninstall? (N/y)".format(elm))
+                        if remove == "y":
+                            subprocess.call(["brew", "uninstall", elm])
+
+                    else:
+                        print("{} is used by {}...".format(elm, ", ".join(deps)))
+
+        else:
+            print("unknown option: {}\n".format(arg))
+            print_help()
 
 
 def print_help():
